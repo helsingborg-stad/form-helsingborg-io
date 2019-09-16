b3d2646a9f2c883f3e3f54b2d96127e01d592f4a
Teddy Andersson <teddyandersson@Teddys-MacBook-Pro.local>
2019-09-16 16:45:23 +0200

    Tests for get requests on forms endpoint
    Success tests have been added for endpoints /forms and /forms/:id

---
+++ src/components/form/form.test.js
@@ -0,0 +1,37 @@
+/* eslint-disable no-unused-expressions */
+require('dotenv').config({ path: './.env.test' });
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../server');
+
+const should = chai.should();
+chai.use(chaiHttp);
+
+describe('Form', () => {
+  after(async () => {
+    server.close();
+  });
+
+  beforeEach(async () => {
+  });
+
+  it('should return json on GET with faked query', async () => chai
+    .request(server)
+    .get('/forms')
+    .send()
+    .then((res) => {
+      res.should.have.status(200);
+      res.should.be.json;
+      should.exist(res.body);
+    }));
+
+  it('should return json on GET with correct query params at fake', async () => chai
+    .request(server)
+    .get('/forms/1')
+    .send()
+    .then((res) => {
+      res.should.have.status(200);
+      res.should.be.json;
+      should.exist(res.body);
+    }));
+});
