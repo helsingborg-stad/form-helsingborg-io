d5e2dd3c2774747bc46c5674057966b2cbedfd4e
Teddy Andersson <teddyandersson@Teddys-MacBook-Pro.local>
2019-09-19 09:10:28 +0200

    Updates setup by following the boilerplate repo

--- setup.js
+++ setup.js
@@ -1,7 +1,20 @@
 const fs = require('fs');
 
-fs.createReadStream('example.env')
-  .pipe(fs.createWriteStream('.env'));
+const sourceFile = '.env-example';
+const createFiles = ['.env', '.env.test'];
 
-fs.createReadStream('example.env')
-  .pipe(fs.createWriteStream('.env.test'));
+createFiles.forEach((createFile) => {
+  try {
+    if (!fs.existsSync(createFile)) {
+      fs.createReadStream(sourceFile).pipe(fs.createWriteStream(createFile));
+      console.log(`Created file ${createFile} from sourceFile`);
+    } else {
+      console.error(`File ${createFile} already exists.`);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+// Done
+console.log('Setup completed. Additional log information above.');
