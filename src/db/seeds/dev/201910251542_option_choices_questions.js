3343796ad1e9852851e630c582b1914d545b6b0a
Teddy Andersson <teddyandersson@Teddys-MBP.th1ng.se>
2019-10-31 11:24:56 +0100

    Changed options seed data.
    The seed data for options was incorecct and caused issues with rendering in the application.

--- src/db/seeds/dev/201910251542_option_choices_questions.js
+++ src/db/seeds/dev/201910251542_option_choices_questions.js
@@ -5,11 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('option_choices_questions').insert([
-        {question_id: 10, option_choice_id: 1},
-        {question_id: 10, option_choice_id: 2},
+        {question_id: 9, option_choice_id: 1},
+        {question_id: 9, option_choice_id: 2},
         {question_id: 6, option_choice_id: 24},
         {question_id: 6, option_choice_id: 25},
-
       ]);
     });
 };
