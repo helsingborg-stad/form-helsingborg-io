fc097b7d62be2e9a4d339cc1e7c9a09c1fae4824
Teddy Andersson <teddyandersson@Teddys-MacBook-Pro.local>
2019-11-05 16:18:44 +0100

    Mockdata for logics on questions
    The database seeds files have been updated with new mockdata for retreiving logics in order to have dependent questions.

--- src/db/seeds/dev/201911041530_logics.js
+++ src/db/seeds/dev/201911041530_logics.js
@@ -5,7 +5,10 @@ exports.seed = function(knex, Promise) {
     .then(function () {
       // Inserts seed entries
       return knex('logics').insert([
-        { trigger_question: 1, logic_type: 'question' },
+        { trigger_question: 3, logic_type: 'question' },
+        { trigger_question: 7, logic_type: 'question' },
+        { trigger_question: 10, logic_type: 'question' },
+        { trigger_question: 16, logic_type: 'question' },
       ]);
     });
 };
--- src/db/seeds/dev/201911041531_logic_actions.js
+++ src/db/seeds/dev/201911041531_logic_actions.js
@@ -8,7 +8,49 @@ exports.seed = function(knex, Promise) {
         {
           condition: 'yes;yes;equal',
           logic_id: 1,
-          target_question: 2,
+          target_question: 4,
+          action_type: 'add'
+        },
+        {
+          condition: 'yes;yes;equal',
+          logic_id: 1,
+          target_question: 5,
+          action_type: 'add'
+        },
+        {
+          condition: 'yes;yes;equal',
+          logic_id: 1,
+          target_question: 6,
+          action_type: 'add'
+        },
+        {
+          condition: 'other;other;equal',
+          logic_id: 2,
+          target_question: 8,
+          action_type: 'add'
+        },
+        {
+          condition: 'no;no;equal',
+          logic_id: 3,
+          target_question: 11,
+          action_type: 'add'
+        },
+        {
+          condition: 'yes;yes;equal',
+          logic_id: 3,
+          target_question: 11,
+          action_type: 'add'
+        },
+        {
+          condition: 'yes;yes;equal',
+          logic_id: 3,
+          target_question: 12,
+          action_type: 'add'
+        },
+        {
+          condition: 'no;no;equal',
+          logic_id: 4,
+          target_question: 17,
           action_type: 'add'
         },
       ]);
